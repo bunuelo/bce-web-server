3eebf70658f6cfc2af3f7d9e00d31831efe6ea5a
Bo Morgan <bo.morgan@pm.me>
2024-03-24 16:43:37 -0700

    working on deploy

--- bce-web-server/src/routes/+page.svelte
+++ bce-web-server/src/routes/+page.svelte
@@ -2,3 +2,5 @@
 <p>The BCE Web Server is an example web-based client application that accesses the BCE REST API.</p>
 <p>See the <a href="/docs">developer documentation on this server</a> for a developer reference.</p>
 <p>See the <a href="http://64.23.144.229:8000/docs">BCE REST API OpenAPI documentation</a>.</p>
+
+<input type="range" min="50" max="80" value="50" class="slider" id="ipd">
