ecd12b1868b17e91780ea3c1e2e6bce7fdebbf2f
Bo Morgan <bo.morgan@pm.me>
2024-03-24 16:51:56 -0700

    working on deploy

--- bce-web-server/src/routes/+page.svelte
+++ bce-web-server/src/routes/+page.svelte
@@ -1,6 +1,11 @@
+<script>
+  let counter = 1;
+</script>
+
 <h1>BCE</h1>
 <p>The BCE Web Server is an example web-based client application that accesses the BCE REST API.</p>
 <p>See the <a href="/docs">developer documentation on this server</a> for a developer reference.</p>
 <p>See the <a href="http://64.23.144.229:8000/docs">BCE REST API OpenAPI documentation</a>.</p>
 
-<input type="range" min="50" max="80" value="50" class="slider" id="ipd">
+<input type="number" bind:value="{counter}" min="50" max="80" />
+<input type="range" bind:value="{counter}" min="50" max="80" />
