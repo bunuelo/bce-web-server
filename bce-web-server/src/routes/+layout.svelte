800f71ea145a1fc0308b3314a02339f160fd13be
Bo Morgan <bo.morgan@pm.me>
2024-03-24 16:19:27 -0700

    working on deploy

--- bce-web-server/src/routes/+layout.svelte
+++ bce-web-server/src/routes/+layout.svelte
@@ -1,7 +1,17 @@
-<nav>
-  <a href="/">home</a>
-  <a href="/terms">terms</a>
-  <a href="/contact">contact</a>
-</nav>
+<table width="100%">
+  <tr>
+    <td width="100%">
+      <nav>
+	<a href="/">home</a>
+	<a href="/terms">terms</a>
+	<a href="/contact">contact</a>
+      </nav>
+    </td>
+  </tr>
+  <tr>
+    <td width="100%">
+      <slot />
+    </td>
+  </tr>
+</table>
 
-<slot />
