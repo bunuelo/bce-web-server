b2967dddc42982d2251cd9c6b4b426a26433cb67
Bo Morgan <bo.morgan@pm.me>
2024-03-26 21:18:44 -0700

    working on deploy

--- bce-web-server/src/routes/+layout.svelte
+++ bce-web-server/src/routes/+layout.svelte
@@ -7,8 +7,7 @@
   const menu = createMenu({ label: 'Actions' })
 
   function onSelect(e: Event) {
-  console.log("select", (e as CustomEvent).detail)
-    menu.expanded = !menu.expanded
+    console.log("select", (e as CustomEvent).detail)
   }
 
   // prettier-ignore
@@ -34,7 +33,7 @@
 	on:select={onSelect}
 	class="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
 	>
-	Options
+	Menu
 	<Edit class="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100" />
       </button>
       
