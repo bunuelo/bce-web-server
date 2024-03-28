ceac62377c0bba102bec92b2b47cb7a87aeb4a76
Bo Morgan <bo.morgan@pm.me>
2024-03-28 14:59:58 -0700

    working on deploy

--- bce-web-server/src/routes/+page.svelte
+++ bce-web-server/src/routes/+page.svelte
@@ -34,6 +34,7 @@
     }));
     const response_json = await response.json();
     bce_rest_api_message = response_json.message
+    current_image_url = ""
     current_image_url = response_json.image_url
     return response_json;
   }
