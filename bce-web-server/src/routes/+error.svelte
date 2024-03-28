94eedd3b5277478bee36296f0af4b3fc42140f36
Bo Morgan <bo.morgan@pm.me>
2024-03-28 12:15:25 -0700

    working on deploy

--- bce-web-server/src/routes/+error.svelte
+++ bce-web-server/src/routes/+error.svelte
@@ -2,4 +2,9 @@
   import { page } from '$app/stores';
 </script>
 
-<h1>{$page.status} {$page.error.message}</h1>
+<h1>Page does not exist</h1>
+
+<p>Error code #{$page.status}, "{$page.error.message}".</p>
+
+<p>Please either use your browser's back button or use the nagitation
+menu on the left.</p>
