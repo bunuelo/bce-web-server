3e9cd3a64d74d4b37aa4c868e40e83bb90cbc3d1
Bo Morgan <bo.morgan@pm.me>
2024-03-29 12:01:09 -0700

    working on deploy

--- bce-web-server/src/routes/+error.svelte
+++ bce-web-server/src/routes/+error.svelte
@@ -2,7 +2,11 @@
   import { page } from '$app/stores';
 </script>
 
+{#if page.status == 404}
 <h1>Page does not exist</h1>
+{+else}
+<h1>Something went wrong</h1>
+{/if}
 
 <p>Error code #{$page.status}, "{$page.error.message}".</p>
 
