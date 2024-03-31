62574aef7b184e75f2a930790185b263c7dfd620
Bo Morgan <bo.morgan@pm.me>
2024-03-31 14:57:35 -0700

    working on deploy

--- bce-web-server/src/routes/+page.svelte
+++ bce-web-server/src/routes/+page.svelte
@@ -122,7 +122,7 @@
 	      {#each (options ? options.sbc.options : []) as sbc_option}
 	      <tr>
 		<td>
-		  <input bind:this={sbc_option.component} type="radio" id="sbc_{sbc_option.name}" bind:group={sbc} name="sbc" value="{sbc_option.name}" /><label for="sbc_{sbc_option.name}">{sbc_option.display_name}</label>
+		  <input type="radio" id="sbc_{sbc_option.name}" bind:group={sbc} name="sbc" value="{sbc_option.name}" /><label for="sbc_{sbc_option.name}">{sbc_option.display_name}</label>
 		</td>
 		<td align="right">
 		  ${sbc_option.price.toFixed(2)}
