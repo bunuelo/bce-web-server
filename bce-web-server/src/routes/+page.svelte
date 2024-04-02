5172d528a592b2e6787cea29a3bc237932080474
Bo Morgan <bo.morgan@pm.me>
2024-04-01 17:41:20 -0700

    working on deploy

--- bce-web-server/src/routes/+page.svelte
+++ bce-web-server/src/routes/+page.svelte
@@ -173,7 +173,7 @@
 		  <input type="radio" id="sbc_{sbc_option.name}" bind:group={sbc} name="sbc" value="{sbc_option.name}" /><label for="sbc_{sbc_option.name}">{sbc_option.display_name}</label>
 		</td>
 		<td align="right">
-		  ${sbc_option.price.toFixed(2)}
+		  ${(sbc ? sbc.price - sbc_option.price : 0).toFixed(2)}
 		</td>
 	      </tr>
 	      {/each}
