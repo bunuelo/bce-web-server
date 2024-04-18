18403cf7dd22e144055d1eed27a4ca8e517134c8
Bo Morgan <bo.morgan@pm.me>
2024-04-17 17:17:41 -0700

    developing

--- bce-web-server/src/routes/bce_session.js
+++ bce-web-server/src/routes/bce_session.js
@@ -7,7 +7,7 @@ export default class BceSession {
 	const d = new Date();
 	d.setTime(d.getTime() + (exdays*24*60*60*1000));
 	let expires = "expires="+ d.toUTCString();
-	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;samesite=strict";
     }
 
     get_cookie(cname) {
