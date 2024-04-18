2496f1317d8439fa02e3f98b3601ac8e72223bee
Bo Morgan <bo.morgan@pm.me>
2024-04-17 17:59:04 -0700

    developing

--- bce-web-server/src/routes/user/login/+page.svelte
+++ bce-web-server/src/routes/user/login/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { onMount } from 'svelte';
   import BceRestApi from "../../bce_rest_api.js";
   import BceSession from "../../bce_session.js";
   let bce_rest_api = new BceRestApi();
@@ -6,10 +7,15 @@
 
   let email = "";
   let password = "";
+  let session_token = "";
+  
+  onMount(async () => {
+    session_token = bce_session.get_cookie("session_token")
+  });
   
   async function login() {
     console.log("Login: here.");
-    let session_token = await bce_rest_api.user_login(email, password);
+    session_token = await bce_rest_api.user_login(email, password);
     if (session_token != null) {
       console.log("Login: session_token = " + session_token);
       bce_session.set_cookie("session_token", session_token, 1);
@@ -44,6 +50,13 @@
     </td>
     <td>
       <button type="button" on:click={login}>Login</button>
+    </td>
+  </tr>
+  <tr>
+    <td>
+    </td>
+    <td>
+      Session Token: {session_token}
     </td>
   </tr>
 </table>
