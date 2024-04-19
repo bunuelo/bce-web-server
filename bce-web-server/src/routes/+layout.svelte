b0fc851316f78748212d7e187bb94d5ec817a94e
Bo Morgan <bo.morgan@pm.me>
2024-04-19 13:21:31 -0700

    developing

--- bce-web-server/src/routes/+layout.svelte
+++ bce-web-server/src/routes/+layout.svelte
@@ -2,21 +2,14 @@
   import Logo from './Logo.svelte'
   import Menu from './Menu.svelte'
   import { alert } from './bce_stores.js'
-  import { onDestroy } from 'svelte'
-  
-  let alertContent = ''
-  
-  const unsubscribe = alert.subscribe((value) => alertContent = value)
-  
-  onDestroy(unsubscribe)
   
 </script>
 
 <link rel="icon" href="/favicon.png?v=2" />
 
-{#if alertContent}
-<div on:click={() => alertContent = ''}>
-  <p>{ alertContent }</p>
+{#if $alert}
+<div on:click={() => $alert = ''}>
+  <p>{ $alert }</p>
 </div>
 {/if}
 
