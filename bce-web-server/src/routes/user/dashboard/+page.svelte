5a8e18b7e63f610489ec16db96690644254c0908
Bo Morgan <bo.morgan@pm.me>
2024-04-19 15:31:30 -0700

    developing

--- bce-web-server/src/routes/user/dashboard/+page.svelte
+++ bce-web-server/src/routes/user/dashboard/+page.svelte
@@ -1,3 +1,17 @@
+<script>
+    import { onMount } from 'svelte';
+    import { goto } from '$app/navigation';
+    
+    let password;
+    
+    onMount(async () => {
+        $user_session_is_valid = await bce_session.session_is_valid()
+        if ($user_session_is_valid) {
+            goto("/user/dashboard");
+        }
+    });
+</script>
+  
 <h1>User Dashboard</h1>
 
 
