a83dfc0a02fabe309124452742ae1edbeb1ac142
Bo Morgan <bo.morgan@pm.me>
2024-04-21 12:58:51 -0700

    developing

---
+++ bce-web-server/src/lib/bce_inventory.js
@@ -0,0 +1,18 @@
+import { get } from 'svelte/store'
+import { user_email } from './bce_stores.js'
+import { user_session_token } from './bce_stores.js'
+import { user_session_is_valid } from './bce_stores.js'
+import { user_security_level } from './bce_stores.js'
+import BceRestApi from "./bce_rest_api.js";
+let bce_rest_api = new BceRestApi();
+
+export default class BceInventory {
+    
+    constructor() {
+    }
+
+    async products() {
+        return []
+    }
+  
+}
