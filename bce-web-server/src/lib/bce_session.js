9714536f9a56c31e8fa56f6b7a075f8cf4eea903
Bo Morgan <bo.morgan@pm.me>
2024-04-20 18:19:06 -0700

    developing

--- bce-web-server/src/lib/bce_session.js
+++ bce-web-server/src/lib/bce_session.js
@@ -1,6 +1,7 @@
 import { get } from 'svelte/store'
 import { user_email } from './bce_stores.js'
 import { user_session_token } from './bce_stores.js'
+import { user_session_is_valid } from './bce_stores.js'
 import { user_security_level } from './bce_stores.js'
 import BceRestApi from "./bce_rest_api.js";
 let bce_rest_api = new BceRestApi();
