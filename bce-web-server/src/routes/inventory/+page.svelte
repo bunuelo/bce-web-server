e85307c7df1a92452d409cacb89a068690160634
Bo Morgan <bo.morgan@pm.me>
2024-04-23 16:25:42 -0700

    developing

--- bce-web-server/src/routes/inventory/+page.svelte
+++ bce-web-server/src/routes/inventory/+page.svelte
@@ -82,7 +82,7 @@
         </td>
     </tr>
     {#key product_update_count}
-      {#each products as product}
+      {#each products as product (product.product_id)}
         <tr style="border: 1px solid black; border-collapse: collapse;">
             <td style="border: 1px solid black; border-collapse: collapse;">
                 {#if edit_product_id == product.product_id}
