0671dc1fa1476ee4088ca97f90a361ebeaa9bf57
Bo Morgan <bo.morgan@pm.me>
2024-04-24 17:38:19 -0700

    developing

--- bce-web-server/src/routes/inventory/+page.svelte
+++ bce-web-server/src/routes/inventory/+page.svelte
@@ -111,7 +111,7 @@
             </td>
             <td align="right" style="border: 1px solid black; border-collapse: collapse;">
                 {#if edit_product_id == product.product_id}
-	          <input type="number" bind:value="{product.sale_price}" min=0 style="width: 90%;" on:change={on_input_sale_price_change} />
+	          <input type="number" data-type="currency" bind:value="{product.sale_price}" min=0 style="width: 90%;" on:change={on_input_sale_price_change} />
                 {:else}
                   { product.sale_price }
                 {/if}
@@ -134,6 +134,9 @@
           </td>
           <td align="right" style="border: 1px solid black; border-collapse: collapse;">
 	      <input type="number" bind:value="{new_product_quantity}" min=0 style="width: 90%;" />
+          </td>
+          <td align="right" style="border: 1px solid black; border-collapse: collapse;">
+	      <input type="number" data-type="currency" bind:value="{new_product_sale_price}" min=0 style="width: 90%;" />
           </td>
           <td style="border: 0px;">
               <a href="#" on:click={on_click_create_product}>create</a>
