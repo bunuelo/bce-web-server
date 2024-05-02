dd0c3d7a42053c9141ae9e9154ddd877d734890a
Bo Morgan <bo.morgan@pm.me>
2024-05-02 15:14:01 -0700

    developing

--- bce-web-server/src/routes/docs/+page.svelte
+++ bce-web-server/src/routes/docs/+page.svelte
@@ -1,5 +1,5 @@
 <h1>Docs</h1>
 
 <p>The BCE website is an example web-based client application that accesses the BCE REST API.</p>
-<p>See the <a href="http://64.23.144.229:8000/docs">BCE REST API OpenAPI documentation</a>.</p>
+<p>See the <a href="https://braincomputerenterprises.com:8000/docs">BCE REST API OpenAPI documentation</a>.</p>
 
