429635c7cfb2dbf81c84f5de9abf80f743f55b94
Bo Morgan <bo.morgan@pm.me>
2024-05-01 18:39:36 -0700

    developing

--- bce-web-server/src/routes/design/+page.svelte
+++ bce-web-server/src/routes/design/+page.svelte
@@ -3,14 +3,11 @@
     let bce_session = new BceSession();
     import Design from "./Design.svelte";
 
-    let access_granted = false;
-    
     onMount(async () => {
         $user_security_level = await bce_session.security_level()
         if ($user_security_level < 25) {
             goto("/user/dashboard");
         }
-        access_granted = true;                           
     });
 
 </script>
