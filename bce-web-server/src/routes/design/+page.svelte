e036c3b2e29f8cc9863c51f5c90b7c1ffb4cbd89
Bo Morgan <bo.morgan@pm.me>
2024-05-01 18:41:08 -0700

    developing

--- bce-web-server/src/routes/design/+page.svelte
+++ bce-web-server/src/routes/design/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import BceSession from "$lib/bce_session.js";
     let bce_session = new BceSession();
+    import { user_security_level } from '$lib/bce_stores.js'
     import Design from "./Design.svelte";
 
     onMount(async () => {
