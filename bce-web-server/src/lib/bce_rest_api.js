19bd11033cd0a0c036991e5ee2977890b58738d0
Bo Morgan <bo.morgan@pm.me>
2024-05-02 14:53:46 -0700

    developing

--- bce-web-server/src/lib/bce_rest_api.js
+++ bce-web-server/src/lib/bce_rest_api.js
@@ -1,7 +1,7 @@
 export default class BceRestApi {
     
     constructor() {
-	this.apiURL = "https://64.23.144.229:8000";
+	this.apiURL = "https://braincomputerenterprises.com:8000";
 	this.message = "";
     }
     
