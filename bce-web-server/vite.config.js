2e63923cc60a54def3348bf6fa4a790565550146
Bo Morgan <bo.morgan@pm.me>
2024-05-02 14:11:31 -0700

    developing

--- bce-web-server/vite.config.js
+++ bce-web-server/vite.config.js
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, '/etc/letsencrypt/live/braincomputerenterprises.com/privkey.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, '/etc/letsencrypt/live/braincomputerenterprises.com/fullchain.pem')),
+    },
+    // Make sure the server is accessible over the local network
+    host: '0.0.0.0',
+  },
+  plugins: [sveltekit()]
 });
