51ebbfa80d028cc6af771e960c5682c6f78f44de
Bo Morgan <bo.morgan@pm.me>
2024-05-05 16:33:44 -0700

    developing

--- bce-web-server/src/routes/+layout.svelte
+++ bce-web-server/src/routes/+layout.svelte
@@ -2,8 +2,12 @@
     import Logo from './Logo.svelte'
     import Menu from './Menu.svelte'
     import { alert } from '$lib/bce_stores.js'
-  
+      
 </script>
+
+<svelte:head>
+    <title>BCE</title>
+</svelte:head>
 
 <link rel="icon" href="/favicon.png?v=2" />
 
