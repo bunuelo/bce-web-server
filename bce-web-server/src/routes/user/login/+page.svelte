ce379ed1f14c4cc3432c4de5f8dfde12b83df57e
Bo Morgan <bo.morgan@pm.me>
2024-05-07 18:31:07 -0700

    developing

--- bce-web-server/src/routes/user/login/+page.svelte
+++ bce-web-server/src/routes/user/login/+page.svelte
@@ -24,10 +24,8 @@
     });
   
   async function login() {
-      console.log("Login: here.");
       $user_session_token = await bce_rest_api.user_login($user_email, password);
       if ($user_session_token != null) {
-          console.log("Login: session_token = " + $user_session_token);
           bce_session.login($user_email, $user_session_token);
           $user_session_is_valid = true;
           $user_security_level = await bce_session.security_level();
@@ -37,7 +35,6 @@
           $user_email = "";
           password = "";
           $alert = "Login failed.";
-          console.log("Login failed.");
       }
   }
   
