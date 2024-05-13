9b79f477da19d82e8cab4b245a0ee8abe2b2ffcb
Bo Morgan <bo.morgan@pm.me>
2024-05-13 16:16:32 -0700

    developing

--- bce-web-server/src/routes/user/data/+page.svelte
+++ bce-web-server/src/routes/user/data/+page.svelte
@@ -3,6 +3,7 @@
     import { goto } from '$app/navigation';
     import { alert } from '$lib/bce_stores.js'
     import { user_session_is_valid } from '$lib/bce_stores.js'
+    import { user_security_level } from '$lib/bce_stores.js'
     import BceSession from "$lib/bce_session.js";
     let bce_session = new BceSession();
 
@@ -51,11 +52,12 @@
 
 </script>
 
+
 <svelte:head>
     <title>User Data</title>
 </svelte:head>
 
-{#if $user_session_is_valid}
+{#if $user_session_is_valid && $user_security_level >= 25}
 
   <h1>User Data</h1>
   
