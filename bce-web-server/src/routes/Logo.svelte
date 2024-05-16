c24866445db41d61af97889e63bc366f019eb70c
Bo Morgan <bo.morgan@pm.me>
2024-05-16 16:29:43 -0700

    developing

--- bce-web-server/src/routes/Logo.svelte
+++ bce-web-server/src/routes/Logo.svelte
@@ -1,1 +1,1 @@
-<img src="/BCE-web.png" style="width: 150px;" alt="BCE Logo">
+<img src="/BCE-web.png" style="width: 150px;" alt="BCE Logo" aria-description="The BCE Logo is a black linedrawing of a human brain with white text overlaid that reads BCE.">
