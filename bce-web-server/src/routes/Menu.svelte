6083760de12ddba17623e6d72b04a3c5a8800c16
Bo Morgan <bo.morgan@pm.me>
2024-05-16 17:08:32 -0700

    developing

--- bce-web-server/src/routes/Menu.svelte
+++ bce-web-server/src/routes/Menu.svelte
@@ -16,18 +16,19 @@
     
 </script>
 
-<ul aria-description="The Top-level Menu for the BCE Webpage">
-    <li>
-	<a href="/">home</a>
-    </li>
-    {#if $user_session_is_valid}
-      <li>
-	  <a href="/user/dashboard">user dashboard</a>
-      </li>
-      {#if $user_security_level >= 25}
+<nav>
+    <ul>
         <li>
-	    <a href="/user/acls">user ACLs</a>
+	    <a href="/">home</a>
         </li>
+        {#if $user_session_is_valid}
+          <li>
+	      <a href="/user/dashboard">user dashboard</a>
+          </li>
+          {#if $user_security_level >= 25}
+            <li>
+	        <a href="/user/acls">user ACLs</a>
+            </li>
         <li>
 	    <a href="/user/data">user data</a>
         </li>
@@ -39,28 +40,30 @@
       <li>
 	  <a href="/user/login">login</a>
       </li>
-    <li>
-	<a href="/user/create-account">create account</a>
-    </li>
-  {/if}
-  {#if $user_security_level >= 25}
-    <li>
-	<a href="/design">design</a>
-    </li>
-  {/if}
-  {#if $user_security_level >= 100}
-    <li>
-	<a href="/inventory">inventory</a>
-    </li>
-  {/if}
-  <li>
-      <a href="/docs">docs</a>
-  </li>
-    <li>
-	<a href="/terms">terms</a>
-    </li>
-    <li>
-	<a href="/contact">contact</a>
-    </li>
-</ul>
+        <li>
+	    <a href="/user/create-account">create account</a>
+        </li>
+      {/if}
+      {#if $user_security_level >= 25}
+        <li>
+	    <a href="/design">design</a>
+        </li>
+      {/if}
+      {#if $user_security_level >= 100}
+        <li>
+	    <a href="/inventory">inventory</a>
+        </li>
+      {/if}
+      <li>
+          <a href="/docs">docs</a>
+      </li>
+        <li>
+	    <a href="/terms">terms</a>
+        </li>
+        <li>
+	    <a href="/contact">contact</a>
+        </li>
+    </ul>
+</nav>
 
+    
