95f1c18203292c7f9a347e32f71f44b7300edc94
Bo Morgan <bo.morgan@pm.me>
2024-05-18 17:36:24 -0700

    developing

--- bce-web-server/src/routes/user/acls/+page.svelte
+++ bce-web-server/src/routes/user/acls/+page.svelte
@@ -204,7 +204,7 @@
                   {/if}
                   {#if acl.owner}
                     <a href="#" on:click={() => on_click_upload_asset(acl.acl_id)}
-                        aria-label="Upload asset to {acl.display_name}">upload asset</a>
+                        aria-label="Upload assets to {acl.display_name}">upload assets</a>
                     <!-- <input type="file" bind:acl.upload_files multiple /> -->
                   {/if}
               </td>
