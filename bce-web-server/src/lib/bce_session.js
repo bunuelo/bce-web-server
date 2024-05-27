3c9de75dcd3942d9932926b45e19c5690175c8b5
Bo Morgan <bo.morgan@pm.me>
2024-05-27 14:37:42 -0700

    developing

--- bce-web-server/src/lib/bce_session.js
+++ bce-web-server/src/lib/bce_session.js
@@ -3,6 +3,7 @@ import { user_email } from './bce_stores.js'
 import { user_session_token } from './bce_stores.js'
 import { user_session_is_valid } from './bce_stores.js'
 import { user_security_level } from './bce_stores.js'
+import { user_color_theme } from './bce_stores.js'
 import BceRestApi from "./bce_rest_api.js";
 let bce_rest_api = new BceRestApi();
 
