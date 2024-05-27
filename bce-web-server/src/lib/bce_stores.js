e2f8feb9a7f2ed685b4353029aae571fa4dfba68
Bo Morgan <bo.morgan@pm.me>
2024-05-27 12:05:11 -0700

    developing

--- bce-web-server/src/lib/bce_stores.js
+++ bce-web-server/src/lib/bce_stores.js
@@ -5,4 +5,5 @@ export const user_email = writable("");
 export const user_session_token = writable("");
 export const user_session_is_valid = writable(false);
 export const user_security_level = writable(0);
+export const user_color_theme = writable("");
 
--- bce-web-server/src/routes/+layout.svelte
+++ bce-web-server/src/routes/+layout.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
+    import { onMount } from 'svelte';
     import Logo from './Logo.svelte'
     import Menu from './Menu.svelte'
     import { alert } from '$lib/bce_stores.js'
-      
+    import { user_color_theme } from '$lib/bce_stores.js'
+    
+    onMount(async () => {
+        $user_color_theme = await bce_session.session_color_theme()
+        document.documentElement.style.setProperty("color-mode", "light");
+    });
+
 </script>
 
 <link rel="icon" href="/favicon.png?v=2" />
