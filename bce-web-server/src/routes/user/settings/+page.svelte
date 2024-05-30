695d7731491f261ed186f30039ed3d7ea61c86a7
Bo Morgan <bo.morgan@pm.me>
2024-05-30 15:42:16 -0700

    developing

--- bce-web-server/src/routes/user/settings/+page.svelte
+++ bce-web-server/src/routes/user/settings/+page.svelte
@@ -1,9 +1,7 @@
 <script>
     import { onMount } from 'svelte';
     import { goto } from '$app/navigation';
-    import { user_session_is_valid } from '$lib/bce_stores.js'
-    import { user_security_level } from '$lib/bce_stores.js'
-    import { user_color_theme } from '$lib/bce_stores.js'
+    import { user_session_is_valid, user_security_level, user_color_theme, user_language } from '$lib/bce_stores.js'
     import BceSession from "$lib/bce_session.js";
     let bce_session = new BceSession();
     
