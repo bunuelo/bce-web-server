d9a6285767eb46a1b1680956fdf0de7afa713f02
Bo Morgan <bo.morgan@pm.me>
2024-05-31 16:52:56 -0700

    developing

--- bce-web-server/src/routes/docs/+page.svelte
+++ bce-web-server/src/routes/docs/+page.svelte
@@ -10,5 +10,5 @@
 <h1>{bce_lang($user_language, "page_docs_title")}</h1>
 
 <p>{bce_lang($user_language, "page_docs_description")}</p>
-<p>See the <a href="https://bce.center:8000/docs" target="_blank">BCE REST API OpenAPI documentation</a>.</p>
+<p><a href="https://bce.center:8000/docs" target="_blank">{bce_lang($user_language, "page_docs_rest_api_link_description")}</a></p>
 
