36bd01311e6a31817a1f9014db65f8cf40839a68
Bo Morgan <bo.morgan@pm.me>
2024-05-31 12:59:11 -0700

    developing

--- bce-web-server/src/routes/user/dashboard/+page.svelte
+++ bce-web-server/src/routes/user/dashboard/+page.svelte
@@ -3,6 +3,8 @@
     import { goto } from '$app/navigation';
     import { user_session_is_valid } from '$lib/bce_stores.js'
     import { user_security_level } from '$lib/bce_stores.js'
+    import { user_language } from '$lib/bce_stores.js'
+    import { bce_lang } from '$lib/bce_locale.js'
     import BceSession from "$lib/bce_session.js";
     let bce_session = new BceSession();
     
@@ -19,12 +21,12 @@
 </script>
 
 <svelte:head>
-    <title>Dashboard</title>
+    <title>{bce_lang($user_language, "page_dashboard_title")}</title>
 </svelte:head>
 
 {#if $user_session_is_valid}
 
-  <h1>Dashboard</h1>
+  <h1>{bce_lang($user_language, "page_dashboard_title")}</h1>
   
   <ul>
       {#if $user_security_level >= 25}
