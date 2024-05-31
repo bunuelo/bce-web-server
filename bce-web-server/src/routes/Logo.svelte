6b182f50969596538b8274eb249e2ad0470241d0
Bo Morgan <bo.morgan@pm.me>
2024-05-31 12:18:06 -0700

    developing

--- bce-web-server/src/routes/Logo.svelte
+++ bce-web-server/src/routes/Logo.svelte
@@ -1,3 +1,8 @@
+<script lang="ts">
+    import { user_language } from '$lib/bce_stores.js'
+    import { bce_lang } from '$lib/bce_locale.js'
+</script>
+
 <a href="/">
-    <img src="/BCE-web.png" style="width: 150px;" alt="BCE Logo">
+    <img src="{bce_lang($user_language, 'logo_image_path')}" style="width: 150px;" alt="BCE Logo">
 </a>
