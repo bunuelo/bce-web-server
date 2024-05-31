e018d52fdea0a3aa0ff1572dd83177fbaa3d45ff
Bo Morgan <bo.morgan@pm.me>
2024-05-31 16:15:20 -0700

    developing

--- bce-web-server/src/routes/user/assets/+page.svelte
+++ bce-web-server/src/routes/user/assets/+page.svelte
@@ -4,6 +4,8 @@
     import { alert } from '$lib/bce_stores.js'
     import { user_session_is_valid } from '$lib/bce_stores.js'
     import { user_security_level } from '$lib/bce_stores.js'
+    import { user_language } from '$lib/bce_stores.js'
+    import { bce_lang } from '$lib/bce_locale.js'
     import BceSession from "$lib/bce_session.js";
     let bce_session = new BceSession();
 
@@ -44,12 +46,12 @@
 
 
 <svelte:head>
-    <title>User Assets</title>
+    <title>{bce_lang($user_language, "page_assets_title")}</title>
 </svelte:head>
 
 {#if $user_session_is_valid && $user_security_level >= 25}
 
-  <h1>User Assets</h1>
+  <h1>{bce_lang($user_language, "page_assets_title")}</h1>
   
   <label>
       ACL: 
