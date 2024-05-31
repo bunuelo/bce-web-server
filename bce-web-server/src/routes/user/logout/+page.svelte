434ca01cb608a65f15949e82f817539868648ac2
Bo Morgan <bo.morgan@pm.me>
2024-05-31 12:32:01 -0700

    developing

--- bce-web-server/src/routes/user/logout/+page.svelte
+++ bce-web-server/src/routes/user/logout/+page.svelte
@@ -1,22 +1,24 @@
 <script>
-  import { onMount } from 'svelte';
-  import { goto } from '$app/navigation';
-  import { invalidate } from '$app/navigation';
-  import { invalidateAll } from '$app/navigation';
-  import BceRestApi from "$lib/bce_rest_api.js";
-  import BceSession from "$lib/bce_session.js";
-  import { user_session_is_valid } from '$lib/bce_stores.js'
-  import { user_security_level } from '$lib/bce_stores.js'
-  import { alert } from "$lib/bce_stores.js";
-  let bce_rest_api = new BceRestApi();
-  let bce_session = new BceSession();
-
-  onMount(async () => {
-    bce_session.logout()
-    $alert = "Logout successful!";
-    goto("/user/dashboard");
-  });
-  
+    import { onMount } from 'svelte';
+    import { goto } from '$app/navigation';
+    import { invalidate } from '$app/navigation';
+    import { invalidateAll } from '$app/navigation';
+    import BceRestApi from "$lib/bce_rest_api.js";
+    import BceSession from "$lib/bce_session.js";
+    import { user_session_is_valid } from '$lib/bce_stores.js'
+    import { user_security_level } from '$lib/bce_stores.js'
+    import { alert } from "$lib/bce_stores.js";
+    import { user_language } from '$lib/bce_stores.js'
+    import { bce_lang } from '$lib/bce_locale.js'
+    let bce_rest_api = new BceRestApi();
+    let bce_session = new BceSession();
+    
+    onMount(async () => {
+        bce_session.logout()
+        $alert = bce_lang($user_language, "alert_logout_successful");
+        goto("/user/dashboard");
+    });
+    
 </script>
 
 <h1>Logout</h1>
