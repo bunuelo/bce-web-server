2608424d1a4d653e0d13e6af5d5bda2046576629
Bo Morgan <bo.morgan@pm.me>
2024-05-31 16:37:45 -0700

    developing

--- bce-web-server/src/routes/docs/+page.svelte
+++ bce-web-server/src/routes/docs/+page.svelte
@@ -1,8 +1,13 @@
+<script>
+    import { user_language } from '$lib/bce_stores.js'
+    import { bce_lang } from '$lib/bce_locale.js'
+</script>
+
 <svelte:head>
-    <title>Docs</title>
+    <title>{bce_lang($user_language, "page_docs_title")}</title>
 </svelte:head>
 
-<h1>Docs</h1>
+<h1>{bce_lang($user_language, "page_docs_title")}</h1>
 
 <p>The BCE website is an example web-based client application that accesses the BCE REST API.</p>
 <p>See the <a href="https://bce.center:8000/docs" target="_blank">BCE REST API OpenAPI documentation</a>.</p>
