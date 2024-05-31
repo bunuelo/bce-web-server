3a0f49e175ec66eb1a961edd50be4ad2cedb3883
Bo Morgan <bo.morgan@pm.me>
2024-05-31 14:02:53 -0700

    developing

--- bce-web-server/src/routes/user/acls/+page.svelte
+++ bce-web-server/src/routes/user/acls/+page.svelte
@@ -151,7 +151,7 @@
           <td style="text-align:center;"><i>{bce_lang($user_language, "page_acls_label_owner")}</i></td>
           <td style="text-align:center;"><i>{bce_lang($user_language, "page_acls_label_read")}</i></td>
           <td style="text-align:center;"><i>{bce_lang($user_language, "page_acls_label_write")}</i></td>
-          <td style="text-align:center;"><i>Public</i></td>
+          <td style="text-align:center;"><i>{bce_lang($user_language, "page_acls_label_public")}</i></td>
           <td style="text-align:center;"><i>Active</i></td>
           <td style="text-align:center;"><i hidden>Action</i></td>
           <td></td>
