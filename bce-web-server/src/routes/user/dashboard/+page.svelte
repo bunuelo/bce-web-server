a925f0b210a7dbfc8a723fb90c3265558554e018
Bo Morgan <bo.morgan@pm.me>
2024-06-01 08:50:04 -0700

    developing

--- bce-web-server/src/routes/user/dashboard/+page.svelte
+++ bce-web-server/src/routes/user/dashboard/+page.svelte
@@ -7,6 +7,8 @@
     import { bce_lang } from '$lib/bce_locale.js'
     import BceSession from "$lib/bce_session.js";
     let bce_session = new BceSession();
+
+    let done_loading = false;
     
     onMount(async () => {
         if (! $user_session_is_valid) {
@@ -16,6 +18,7 @@
             goto("/user/login");
         }
         $user_security_level = await bce_session.security_level();
+        done_loading = true;
     });
 
 </script>
@@ -24,15 +27,15 @@
     <title>{bce_lang($user_language, "page_dashboard_title")}</title>
 </svelte:head>
 
-{#if $user_session_is_valid}
+{#if done_loading}
 
   <h1>{bce_lang($user_language, "page_dashboard_title")}</h1>
   
   <ul>
+      <li>
+	  <a href="/user/settings">{bce_lang($user_language, "page_dashboard_label_settings")}</a>: {bce_lang($user_language, "page_dashboard_description_settings")}
+      </li>
       {#if $user_security_level >= 25}
-        <li>
-	    <a href="/user/settings">{bce_lang($user_language, "page_dashboard_label_settings")}</a>: {bce_lang($user_language, "page_dashboard_description_settings")}
-        </li>
         <li>
 	    <a href="/user/acls">{bce_lang($user_language, "page_dashboard_label_acls")}</a>: {bce_lang($user_language, "page_dashboard_description_acls")}
         </li>
