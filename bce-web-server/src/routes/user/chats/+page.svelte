8a8e527b0a775efccedf5adc639ab49254934e73
Bo Morgan <bo.morgan@pm.me>
2024-09-05 14:55:43 -0700

    adding chats

--- bce-web-server/src/routes/user/chats/+page.svelte
+++ bce-web-server/src/routes/user/chats/+page.svelte
@@ -36,6 +36,17 @@
         chat_selected = "0";
     }
     
+    async function on_click_request_chat_user() {
+        //let success = await bce_session.create_acl(new_acl_display_name);
+        //if (success) {
+        //    $alert = bce_lang($user_language, "page_acls_alert_create_acl_success");
+        //    reset_new_acl();
+        //    await update_acl_list();
+        //} else {
+        $alert = bce_lang($user_language, "page_chats_alert_create_chat_request_failure");
+        //}
+    }
+    
 </script>
 
 
