2ddd62e813104dc6b421758a10833b0f7399040f
Bo Morgan <bo.morgan@pm.me>
2024-09-07 13:05:42 -0700

    working on chat request

--- bce-web-server/src/routes/user/chats/+page.svelte
+++ bce-web-server/src/routes/user/chats/+page.svelte
@@ -56,14 +56,14 @@
     }
     
     async function on_click_request_chat_user() {
-        request_chat_user_email = new_request_chat_user_email
+        let request_chat_user_email = new_request_chat_user_email
+        new_request_chat_user_email = ""
         let success = await bce_session.request_chat(request_chat_user_email);
         if (success) {
             $alert = bce_lang($user_language, "page_chats_alert_create_chat_request_success");
         } else {
             $alert = bce_lang($user_language, "page_chats_alert_create_chat_request_failure");
         }
-        new_request_chat_user_email = ""
         await update_all();
     }
     
