6cf632272f88c4f345a1d7340e10fe1245f7664c
Bo Morgan <bo.morgan@pm.me>
2024-09-07 15:13:22 -0700

    working on chat request

--- bce-web-server/src/routes/user/chats/+page.svelte
+++ bce-web-server/src/routes/user/chats/+page.svelte
@@ -173,9 +173,7 @@
       </tr>
   </table>
   
-  <textarea rows="4" cols="50">
-      Your chat message here.
-  </textarea>
+  <textarea rows="4" cols="50">Your chat message here.</textarea>
   
   <p>{bce_lang($user_language, "page_chats_label_total_assets_count")}: {chats_count}</p>
   
