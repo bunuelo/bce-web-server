8341328113c19a1f98fcaf491cf05ab2f658929a
Bo Morgan <bo.morgan@pm.me>
2024-09-28 05:00:46 -0700

    adding acl to device

--- bce-web-server/src/routes/user/devices/+page.svelte
+++ bce-web-server/src/routes/user/devices/+page.svelte
@@ -53,7 +53,7 @@
     async function update_acl_list() {
         acl_list = await bce_session.acls(true);
         if (acl_selected == "0" && acl_list.length > 0) {
-            acl_selected = acl_list[0];
+            acl_selected = acl_list[0].acl_id;
         }
     }
     
