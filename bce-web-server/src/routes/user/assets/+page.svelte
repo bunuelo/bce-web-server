688b421361193d73babb756ea9b2e118a9139b68
Bo Morgan <bo.morgan@pm.me>
2024-10-04 15:06:56 -0700

    adding evaluations page

--- bce-web-server/src/routes/user/assets/+page.svelte
+++ bce-web-server/src/routes/user/assets/+page.svelte
@@ -29,25 +29,16 @@
     async function update_all() {
     }
     
-    function download_url(url, file_name){
+    function download_url(url){
         var link = document.createElement('a');
         link.href = url;
-        link.download = file_name;
-        document.body.appendChild(link);
         link.click();
-        document.body.removeChild(link);
     }
     
-    //<a href="https://bce.center:8000/asset/download?session_token={$user_session_token}&name={asset.name}&q={Math.round(1000000000* Math.random())}">
-    //{asset.file_name}
-    //</a>
     async function on_asset_select(asset) {
-        console.log("Asset selected: " + asset.name + " (" + asset.file_name + ")");
-        var file_name = asset.file_name.substr(asset.file_name.lastIndexOf('/') + 1);
+        console.log("Download asset: " + asset.name + " (" + asset.file_name + ")");
         var url = "https://bce.center:8000/asset/download?session_token=" + $user_session_token + "&name=" + asset.name + "&file_name=" + file_name;
-        console.log("Downloading asset at: " + url);
-        console.log("file_name = " + file_name);
-        download_url(url, file_name);
+        download_url(url);
     };
     
 </script>
