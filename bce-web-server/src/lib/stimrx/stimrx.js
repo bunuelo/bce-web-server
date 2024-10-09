9a10b648eba8ab73c37167c1329b501acb3ba136
Bo Morgan <bo.morgan@protonmail.com>
2024-10-09 07:53:56 -0700

    adding stimrx.js

---
+++ bce-web-server/src/lib/stimrx/stimrx.js
@@ -0,0 +1,15 @@
+
+export const new_stimrx_sequence_expression = function (children) {
+    return {
+	"type": "SequenceExpression",
+	"children": children
+    };
+}
+
+export const new_stimrx_select_expression = function (children) {
+    return {
+	"type": "SelectExpression",
+	"children": children
+    };
+}
+
