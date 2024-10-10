097a2fec84f63d6c71dfab34096905da15217dce
Bo Morgan <bo.morgan@protonmail.com>
2024-10-10 06:34:36 -0700

    adding StimrxExpressionEditor

--- bce-web-server/src/lib/AssetSelector.svelte
+++ bce-web-server/src/lib/AssetSelector.svelte
@@ -149,7 +149,7 @@
 	        <div class="headerDiv">
 		     <i>{bce_lang($user_language, "component_asset_selector_label_select_an_asset")}</i>
 		</div>
-                <AssetList on_asset_select={on_asset_select} bind:selected_asset={selected_asset} height={popup_div.clientHeight}/>
+                <AssetList on_asset_select={on_asset_select} bind:selected_asset={selected_asset} height={popup_div.clientHeight - 200}/>
                 <div class="cancelDiv">
                     <a href="#" on:click|preventDefault={on_click_cancel}>
                         {bce_lang($user_language, "component_asset_selector_label_cancel")}
