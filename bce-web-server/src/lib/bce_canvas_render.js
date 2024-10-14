982ce69a48dc93747ad238a08d40c34f66bb3c49
Bo Morgan <bo.morgan@protonmail.com>
2024-10-14 14:51:18 -0700

    adding evaluation

--- bce-web-server/src/lib/bce_canvas_render.js
+++ bce-web-server/src/lib/bce_canvas_render.js
@@ -134,6 +134,34 @@ function bce_canvas_render__evaluation_eye(canvas, ctx, color_theme, evaluation,
 
 function bce_canvas_render__blind_spot(canvas, ctx, color_theme, blind_spot) {
     console.log("bce_canvas_render__blind_spot: here.");
+    if (color_theme == "dark") {
+        color_grip = [255, 255, 255];
+    } else {
+        color_grip = [0, 0, 0];
+    }
+    for (var i = 0; i < blind_spot.points.length; i ++) {
+	let point = blind_spot.points[i];
+	
+	let grip_radius = 2.5 * Math.PI / 180.0;
+	let grip_alpha = point.alpha;
+	let grip_omega = point.omega;
+	
+	const center_x             = 0.5 * canvas.width;
+	const center_y             = 0.5 * canvas.height;
+	const maximum_alpha_radius = 0.5 * canvas.height - 1;
+	
+	const maximum_alpha = 45;
+	const alpha_resolution = 5;
+	
+	//ctx.fillStyle = "rgb(" + color_cannot_see[0] + "," + color_cannot_see[1] + "," + color_cannot_see[2] + ")";
+	ctx.strokeStyle = "rgb(" + color_grip[0] + "," + color_grip[1] + "," + color_grip[2] + ")";
+	ctx.beginPath();
+	ctx.arc(center_x + grip_alpha * Math.cos(grip_omega),
+		center_y + grip_alpha * Math.sin(grip_omega),
+		maximum_alpha_radius * grip_radius / maximum_alpha, 0, 2 * Math.PI);
+	//ctx.fill();
+	ctx.stroke();
+    }
 }
 
 export const bce_canvas_render = {
