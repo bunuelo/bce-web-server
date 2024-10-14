2cfbdcc4c2464aceb272368e91e155f1e9f8ec14
Bo Morgan <bo.morgan@protonmail.com>
2024-10-14 15:07:53 -0700

    adding evaluation

--- bce-web-server/src/lib/bce_canvas_render.js
+++ bce-web-server/src/lib/bce_canvas_render.js
@@ -143,14 +143,14 @@ function bce_canvas_render__blind_spot(canvas, ctx, color_theme, blind_spot) {
 	console.log("bce_canvas_render__blind_spot: rendering a point.");
 	let point = blind_spot.points[i];
 	
-	let grip_radius = 2.5 * Math.PI / 180.0;
+	let grip_radius = 1.25 * Math.PI / 180.0;
 	let grip_alpha = point.alpha;
 	let grip_omega = point.omega;
 	
 	const center_x             = 0.5 * canvas.width;
 	const center_y             = 0.5 * canvas.height;
 	const maximum_alpha_radius = 0.5 * canvas.height - 1;
-	const maximum_alpha        = 45;
+	const maximum_alpha        = 45 * Math.PI / 180.0;
 	
 	ctx.fillStyle = "rgb(" + color_grip[0] + "," + color_grip[1] + "," + color_grip[2] + ")";
 	ctx.strokeStyle = "rgb(" + color_grip[0] + "," + color_grip[1] + "," + color_grip[2] + ")";
--- bce-web-server/src/lib/stimrx/stimrx_editor.js
+++ bce-web-server/src/lib/stimrx/stimrx_editor.js
@@ -92,6 +92,14 @@ function new_default_left_eye_stimrx_editor_blind_spot() {
 
 function new_default_right_eye_stimrx_editor_blind_spot() {
     var points = [];
+    let height = 7.5 * Math.PI / 180.0;
+    let width  = 5.5 * Math.PI / 180.0;
+    let center_lateral_angle  = 0.5 * (12.0 + 15.0) * Math.PI / 180.0;
+    let center_vertical_angle = -1.5 * Math.PI / 180.0;
+    let center_alpha = Math.sqrt(center_lateral_angle * center_lateral_angle + center_vertical_angle * center_vertical_angle);
+    let center_omega = Math.atan2(center_vertical_angle, center_lateral_angle);
+    let center = stimrx.new_stimrx_light_angle(center_alpha, center_omega);
+    points.push(center);
     return stimrx_editor.new_stimrx_editor_blind_spot(points);
 }
 
