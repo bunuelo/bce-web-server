1e9062826f6b8dd6cb789cfcbf8f23d4cb48cb76
Bo Morgan <bo.morgan@protonmail.com>
2024-10-15 06:16:24 -0700

    adding evaluation

--- bce-web-server/src/lib/stimrx/stimrx_editor.js
+++ bce-web-server/src/lib/stimrx/stimrx_editor.js
@@ -87,7 +87,7 @@ function new_stimrx_light_angle_from_horizontal_and_vertical_angles(horizontal_a
 
 function new_default_left_eye_stimrx_editor_blind_spot() {
     var points = [];
-    let center_horizontal_angle  = 0.5 * (12.0 + 15.0) * Math.PI / 180.0;
+    let center_horizontal_angle  = -0.5 * (12.0 + 15.0) * Math.PI / 180.0;
     let center_vertical_angle = -1.5 * Math.PI / 180.0;
     let height = 7.5 * Math.PI / 180.0;
     let width  = 5.5 * Math.PI / 180.0;
@@ -104,12 +104,18 @@ function new_default_left_eye_stimrx_editor_blind_spot() {
 
 function new_default_right_eye_stimrx_editor_blind_spot() {
     var points = [];
+    let center_horizontal_angle  = 0.5 * (12.0 + 15.0) * Math.PI / 180.0;
+    let center_vertical_angle = -1.5 * Math.PI / 180.0;
     let height = 7.5 * Math.PI / 180.0;
     let width  = 5.5 * Math.PI / 180.0;
-    let center_horizontal_angle  = -0.5 * (12.0 + 15.0) * Math.PI / 180.0;
-    let center_vertical_angle = -1.5 * Math.PI / 180.0;
-    let center = new_stimrx_light_angle_from_horizontal_and_vertical_angles(center_horizontal_angle, center_vertical_angle);
-    points.push(center);
+    let polygon_side_count = 5;
+    for (var i = 0; i < polygon_side_count; i ++) {
+	let spot_subangle    = (i * 2.0 * Math.PI / polygon_side_count) + 0.5 * Math.PI;
+	let horizontal_angle = center_horizontal_angle + Math.cos(spot_subangle) * width;
+	let vertical_angle   = center_vertical_angle   + Math.sin(spot_subangle) * height;
+	let point            = new_stimrx_light_angle_from_horizontal_and_vertical_angles(horizontal_angle, vertical_angle);
+	points.push(point);
+    }
     return stimrx_editor.new_stimrx_editor_blind_spot(points);
 }
 
