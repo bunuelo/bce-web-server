a99ddb2e136319c296a52c53744257a726fa8112
Bo Morgan <bo.morgan@protonmail.com>
2024-10-15 15:11:30 -0700

    adding evaluation

--- bce-web-server/src/lib/stimrx/StimrxExpressionEditor.svelte
+++ bce-web-server/src/lib/stimrx/StimrxExpressionEditor.svelte
@@ -64,11 +64,18 @@
 	console.log("Mouse down blind spot canvas.  (" + x + "," + y + ")");
     }
 
+    function on_mouseup_blind_spot_canvas(event) {
+	let x = event.pageX;
+	let y = event.pageY;
+	console.log("Mouse up blind spot canvas.  (" + x + "," + y + ")");
+    }
+
     function get_blind_spot_canvas(blind_spot_canvas_id) {
 	if (! (blind_spot_canvas_id in blind_spot_canvas_storage)) {
 	    let canvas = document.createElement("canvas");
 	    document.body.appendChild(canvas);
 	    canvas.addEventListener("mousedown", on_mousedown_blind_spot_canvas, false);
+	    canvas.addEventListener("mouseup", on_mousedown_blind_spot_canvas, false);
 	    blind_spot_canvas_storage[blind_spot_canvas_id] = canvas;
 	}
 	return blind_spot_canvas_storage[blind_spot_canvas_id];
