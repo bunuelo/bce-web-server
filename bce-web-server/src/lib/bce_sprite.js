3226ff38aeeadc64baa3f7eb77ee94887f96a7c6
Bo Morgan <bo.morgan@protonmail.com>
2024-10-17 06:23:50 -0700

    adding bce_sprite

--- bce-web-server/src/lib/bce_sprite.js
+++ bce-web-server/src/lib/bce_sprite.js
@@ -7,13 +7,6 @@ function get_sprite_canvas(canvas_id) {
 	canvas.style.border = "1px solid blue";
 	document.body.appendChild(canvas);
 	canvas.canvas_id = canvas_id;
-	canvas.drag_start_x = null;
-	canvas.drag_start_y = null;
-	canvas.drag_canvas_start_x = null;
-	canvas.drag_canvas_start_y = null;
-	canvas.addEventListener("mousedown",       function(event) {return       on_mousedown_blind_spot_canvas(event, canvas);}, false);
-	canvas.addEventListener("mouseup",   async function(event) {return await on_mouseup_blind_spot_canvas(event, canvas);}, false);
-	canvas.addEventListener("mousemove",       function(event) {return       on_mousemove_blind_spot_canvas(event, canvas);}, false);
 	sprite_canvas_storage[canvas_id] = canvas;
     }
     return sprite_canvas_storage[canvas_id];
