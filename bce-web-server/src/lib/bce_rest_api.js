b64ad96ba4e8798273b7fd41622ef20a6b00a2da
Bo Morgan <bo.morgan@protonmail.com>
2024-10-20 11:57:24 -0700

    adding StimrxEditor.svelte

--- bce-web-server/src/lib/bce_rest_api.js
+++ bce-web-server/src/lib/bce_rest_api.js
@@ -283,13 +283,14 @@ export default class BceRestApi {
 
     async asset_upload(session_token, acl_id, name, file, file_name) {
 	console.log("asset_upload: name = \"" + name + "\", file_name = \"" + file_name + "\"");
+	if (name === null) {
+	    name = "";
+	}
         var data = new FormData();
         data.append("session_token", session_token);
         data.append("acl_id",        Number(acl_id));
-	if (name !== null) {
-            data.append("name", name);
-	}
-        data.append("file", file, file_name);
+        data.append("name",          name);
+        data.append("file",          file, file_name);
         const response_json = await this.fetch_json_with_options("/asset/upload", {
             method: 'POST',
             body: data
