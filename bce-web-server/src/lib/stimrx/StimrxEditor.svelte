1678e61179934c85883466accf15f5a0b8609e1a
Bo Morgan <bo.morgan@protonmail.com>
2024-10-20 11:45:40 -0700

    adding StimrxEditor.svelte

--- bce-web-server/src/lib/stimrx/StimrxEditor.svelte
+++ bce-web-server/src/lib/stimrx/StimrxEditor.svelte
@@ -128,7 +128,8 @@
     }
 
     async function save_prescription(asset_name, rx) {
-	const blob = new Blob([JSON.stringify(rx)], {type: "application/json"});
+	const file_name = "assets/rx.json";
+	const blob      = new Blob([JSON.stringify(rx)], {type: "application/json"});
         bce_session.asset_upload(acl_selected, asset_name, blob, file_name)
             .then(async function (result) {
                 if (!result) {
@@ -147,8 +148,7 @@
 
     async function on_click_add_prescription() {
 	if (editor !== null) {
-	    const rx        = stimrx_editor.new_default_stimrx_editor_prescription();
-	    const file_name = "assets/rx.json";
+	    const rx = stimrx_editor.new_default_stimrx_editor_prescription();
             $alert = bce_lang($user_language, "component_stimrx_expression_editor_alert_upload_asset_in_progress");
 	    await save_prescription(null, rx);
 	}
